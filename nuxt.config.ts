d18fefb2761bd35ca21cbc89b996ba2f58d783d9
pgaytan <pgaytan@controlla.com.mx>
2022-11-22 13:00:35 -0600

    feat: add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        file: "en.json",
+      },
+      {
+        code: "es",
+        file: "es.json",
+      },
+    ],
+    langDir: "lang/",
+    defaultLocale: "es",
+  },
 });
