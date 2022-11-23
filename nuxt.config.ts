2d94b7b08decb7e2709fc385e5abad50fe2d6084
pgaytan <pgaytan@controlla.com.mx>
2022-11-23 09:50:53 -0600

    feat: add nuxt image module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/image-edge",
+    "@nuxtjs/i18n",
+  ],
   i18n: {
     locales: [
       {
