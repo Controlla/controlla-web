0445ac646bd47aba33d07b131dcd93b8245c9d00
gdelgado <gdelgado@controlla.com.mx>
2023-02-02 16:04:32 -0600

    fix: texts

--- components/us/SectionFour.vue
+++ components/us/SectionFour.vue
@@ -2,9 +2,9 @@
     <section class="w-screen overflow-y-hidden">
         <div class="w-screen min-h-[50%] overflow-y-hidden pt-6">
             <div class="md:flex justify-center w-screen">
-                <h2 class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"><strong class="text-7xl text-sky-500">25+</strong> <br><br> Team members</h2>
+                <h2 class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"><strong class="text-7xl text-sky-500">40+</strong> <br><br> Team members</h2>
                 <h2 class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"><strong class="text-7xl text-sky-500">125+</strong><br><br> Delivered projects</h2>
-                <h2 class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"><strong class="text-7xl text-sky-500">4+</strong><br><br> Years</h2>
+                <h2 class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"><strong class="text-7xl text-sky-500">5+</strong><br><br> Years</h2>
             </div>
         </div>
         <div class="w-screen min-h-[50%] md:pt-12 overflow-y-hidden mb-10">
