e5d3d5cdb9f86d041333d43aee5a2afce7355086
gdelgado <gdelgado@controlla.com.mx>
2023-02-09 12:43:37 -0600

    update team images

--- composables/useImages.ts
+++ composables/useImages.ts
@@ -3,22 +3,22 @@ import type Image from "~/interfaces/Image";
 export default function (): Array<Image> {
   return [
     {
-      src: "/img/team_carousel_1.svg",
+      src: "/img/team_carousel_1.png",
     },
     {
-      src: "/img/team_carousel_2.svg",
+      src: "/img/team_carousel_2.png",
     },
     {
-      src: "/img/team_carousel_3.svg",
+      src: "/img/team_carousel_3.png",
     },
     {
-      src: "/img/team_carousel_4.svg",
+      src: "/img/team_carousel_4.png",
     },
     {
-      src: "/img/team_carousel_5.svg",
+      src: "/img/team_carousel_5.png",
     },
     {
-      src: "/img/team_carousel_6.svg",
+      src: "/img/team_carousel_6.png",
     },
   ];
 }
