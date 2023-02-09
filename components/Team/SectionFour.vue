d3d077ba199605569e71ed9a5fa17304f11e3190
gdelgado <gdelgado@controlla.com.mx>
2023-02-08 18:12:42 -0600

    add success counters

--- components/Team/SectionFour.vue
+++ components/Team/SectionFour.vue
@@ -1,26 +1,7 @@
 <template>
   <section class="w-screen overflow-y-hidden">
     <div class="w-screen min-h-[50%] overflow-y-hidden pt-6">
-      <div class="md:flex justify-center w-screen">
-        <h2
-          class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"
-        >
-          <strong class="text-7xl text-sky-500">40+</strong> <br /><br />
-          Team members
-        </h2>
-        <h2
-          class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"
-        >
-          <strong class="text-7xl text-sky-500">125+</strong><br /><br />
-          Delivered projects
-        </h2>
-        <h2
-          class="text-3xl md:mr-6 lg:mr-12 overflow-y-hidden mt-[2rem] md:mt-0 text-center md:text-left"
-        >
-          <strong class="text-7xl text-sky-500">5+</strong><br /><br />
-          Years
-        </h2>
-      </div>
+      <team-success-counters/>
     </div>
     <div class="w-screen min-h-[50%] md:pt-12 overflow-y-hidden mb-10">
       <h1 class="text-center text-3xl font-bold">
